a3f594b8c301ac1b04de5a7bf542d90a43ff0d01
Sunil Kumar Kashyap <147919215+kumarsunilkashyap@users.noreply.github.com>
2025-03-08 06:21:25 +0000

    update

--- 02_Reactrouter/src/App.jsx
+++ 02_Reactrouter/src/App.jsx
@@ -1,21 +1,32 @@
-import { Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Navbar from "./component/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
+import RootLayout from "./layout/RootLayout";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
+      </Route>
+    )
+  );
   return (
     <>
       <Navbar />
       <div className="container flex justify-center items-center min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/product" element={<Product />} />
-        </Routes>
+        <Routes></Routes>
       </div>
     </>
   );
