a0dbe958c3ed922fba05ab41f0ab7aa5ebaeffe3
Sunil Kumar Kashyap <147919215+kumarsunilkashyap@users.noreply.github.com>
2025-03-08 06:09:46 +0000

    update my pages

--- 02_Reactrouter/src/App.jsx
+++ 02_Reactrouter/src/App.jsx
@@ -1,10 +1,22 @@
-import Navbar from "./component/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Header";
+import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container flex justify-center items-center min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </div>
     </>
   );
 }
